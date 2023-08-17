bd30162a520c5e0465906b018649c2bfc43c74a8
Erim Doğan <miredogan2000@gmail.com>
2023-08-17 13:47:27 +0300

    Data Modeling v1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,5 +23,25 @@ public class DemoApplication {
     @GetMapping
     public List<String> hello() {
         return List.of("Hello", "World");
+    }
+
+    @Entity
+    public class Flights{
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String DepartureAirport;
+        private String ArrivalAirPort;
+        private LocalDateTime arrivalDateTime;
+        private LocalDateTime departureDateTime;
+        double price;
+
+        public Long getId() {
+            return id;
+        }
+        public void setId(Long id) {
+            this.id = id;
+        }
     }
 }
